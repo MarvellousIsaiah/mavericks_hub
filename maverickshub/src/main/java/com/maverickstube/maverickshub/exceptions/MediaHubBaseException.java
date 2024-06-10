250db1a5b252959b8f96101483f9bd5b89b96174
Femi Oladeji <oladejifemi00@gmail.com>
2024-06-10 12:54:22 +0100

    created project

---
+++ maverickshub/src/main/java/com/maverickstube/maverickshub/exceptions/MediaHubBaseException.java
@@ -0,0 +1,7 @@
+package com.maverickstube.maverickshub.exceptions;
+
+public class MediaHubBaseException extends Throwable{
+    public MediaHubBaseException(String message) {
+        super(message);
+    }
+}
---
+++ maverickshub/src/main/java/com/maverickstube/maverickshub/exceptions/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.maverickstube.maverickshub.exceptions;
+
+public class UserNotFoundException extends MediaHubBaseException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
