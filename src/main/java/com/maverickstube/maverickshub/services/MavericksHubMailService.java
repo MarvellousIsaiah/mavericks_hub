32c6c69afb19e7f6912afba12237cebb49165f11
MarvellousIsaiah <isaiahugoeze@gmail.com>
2024-09-14 16:53:37 +0100

    new

--- src/main/java/com/maverickstube/maverickshub/services/MavericksHubMailService.java
+++ src/main/java/com/maverickstube/maverickshub/services/MavericksHubMailService.java
@@ -1,6 +1,6 @@
 package com.maverickstube.maverickshub.services;
 
-import com.maverickstube.maverickshub.config.MailConfig;
+//import com.maverickstube.maverickshub.config.MailConfig;
 import com.maverickstube.maverickshub.dtos.requests.BravoMailRequest;
 import com.maverickstube.maverickshub.dtos.requests.Recipient;
 import com.maverickstube.maverickshub.dtos.requests.SendMailRequest;
@@ -17,24 +17,28 @@ import java.util.List;
 @Service
 @AllArgsConstructor
 public class MavericksHubMailService implements MailService{
-    private final MailConfig mailConfig;
-    public String sendMail(SendMailRequest mailRequest) {
-
-            RestTemplate restTemplate = new RestTemplate();
-            String url =  mailConfig.getMailApiUrl();
-            BravoMailRequest request = new BravoMailRequest();
-            request.setSubject(mailRequest.getSubject());
-            request.setSender(new Sender());
-            request.setRecipients(List.of(new Recipient(mailRequest.getEmail(), mailRequest.getRecipientName())));
-            request.setContent(mailRequest.getContent());
-            HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.APPLICATION_JSON);
-            headers.set("api-key", mailConfig.getMailApikey());
-            headers.set("accept", MediaType.APPLICATION_JSON.toString());
-            RequestEntity<?> httpRequest = new RequestEntity<>(request, headers,  HttpMethod.POST, URI.create(url));
-            ResponseEntity<BravoMailResponse> response = restTemplate.postForEntity(url, httpRequest, BravoMailResponse.class);
-            if(response.getBody()!=null && response.getStatusCode()==HttpStatusCode.valueOf(201)) return "mail sent successfully";
-            else throw new RuntimeException("Error sending mail");
-        }
+    @Override
+    public String sendMail(SendMailRequest request) {
+        return null;
+    }
+//    private final MailConfig mailConfig;
+//    public String sendMail(SendMailRequest mailRequest) {
+//
+//            RestTemplate restTemplate = new RestTemplate();
+////            String url =  mailConfig.getMailApiUrl();
+//            BravoMailRequest request = new BravoMailRequest();
+//            request.setSubject(mailRequest.getSubject());
+//            request.setSender(new Sender());
+//            request.setRecipients(List.of(new Recipient(mailRequest.getEmail(), mailRequest.getRecipientName())));
+//            request.setContent(mailRequest.getContent());
+//            HttpHeaders headers = new HttpHeaders();
+//            headers.setContentType(MediaType.APPLICATION_JSON);
+//            headers.set("api-key", mailConfig.getMailApikey());
+//            headers.set("accept", MediaType.APPLICATION_JSON.toString());
+//            RequestEntity<?> httpRequest = new RequestEntity<>(request, headers,  HttpMethod.POST, URI.create(url));
+//            ResponseEntity<BravoMailResponse> response = restTemplate.postForEntity(url, httpRequest, BravoMailResponse.class);
+//            if(response.getBody()!=null && response.getStatusCode()==HttpStatusCode.valueOf(201)) return "mail sent successfully";
+//            else throw new RuntimeException("Error sending mail");
+//        }
     }
 
